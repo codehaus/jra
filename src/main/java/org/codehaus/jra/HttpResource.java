35807a40ce961843fee6522e47a10693808f558e
dandiep <dandiep@e4b056db-521f-0410-b145-f7e5503d33c2>
2006-10-20 21:18:04 +0000

    Recenter around HTTP, add junit to POM, and add more WSDL2 stuff

---
+++ src/main/java/org/codehaus/jra/HttpResource.java
@@ -0,0 +1,14 @@
+package org.codehaus.jra;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HttpResource {
+    String location() default "";
+    String queryParameters() default "";
+    String queryParameterSeparator() default "&";
+    String inputSerialization() default "";
+    String outputSerialization() default "";
+    
+}
